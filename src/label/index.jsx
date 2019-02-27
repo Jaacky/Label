e46fcc1852cef2923dec2791825418c75dec28c9
Jacky <jaackyvan@gmail.com>
2019-02-27 14:53:02 -0800

    Wrapping react component in iFrame

--- src/label/index.jsx
+++ src/label/index.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Frame from 'react-frame-component';
+
+class Main extends React.Component {
+    render() {
+        return (
+            <Frame>
+                <div>Hello</div>
+            </Frame>
+        )
+    }
+}
 
 const app = document.createElement('div');
 app.id = "label-root";
 
 document.body.appendChild(app);
-ReactDOM.render(<div>Hello</div>, app);
+ReactDOM.render(<Main/>, app);
