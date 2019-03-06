9f5c43d40547e7de922100e93fc4b84ec4e8bfa0
Jacky <jaackyvan@gmail.com>
2019-03-05 18:59:50 -0800

    Adding babel plugin to fix missing runtime generator

--- src/background_scripts/background.js
+++ src/background_scripts/background.js
@@ -1,13 +1,34 @@
 const browser = require('webextension-polyfill');
 
+async function toggleLabel(tabId) {
+    const currentTab =
+    tabId === undefined
+        ? (await browser.tabs.query({
+            active: true,
+            currentWindow: true
+        }))[0]
+        : await browser.tabs.get(tabId);
+
+    try {
+        await browser.tabs.executeScript(currentTab.id, {
+            file: '/label.js',
+            runAt: 'document_start',
+            matchAboutBlank: true
+        });
+    } catch(e) {
+        console.log(e);
+    }
+    
+}
 browser.browserAction.onClicked.addListener(() => {
-    console.log("Browser action, label clicked!");
+    toggleLabel();
 });
 
 browser.commands.onCommand.addListener(command => {
     switch(command) {
         case 'toggleLabel':
             console.log("Toggle Label command!");
+            toggleLabel();
             break;
         default:
             console.log(`Unknown command: ${command}`);
